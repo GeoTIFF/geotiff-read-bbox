29f01c676b127a491795586679e64414f72a2db7
DanielJDufour <daniel.j.dufour@gmail.com>
2022-10-09 11:56:03 -0400

    updated deps

--- test.mjs
+++ test.mjs
@@ -1,6 +1,6 @@
-const test = require("flug");
-const GeoTIFF = require("geotiff");
-const readBoundingBox = require("./index");
+import test from "flug";
+import * as GeoTIFF from "geotiff";
+import readBoundingBox from "./index.js";
 
 test("url", async ({ eq }) => {
   console.time("url");
@@ -24,7 +24,7 @@ test("url", async ({ eq }) => {
 test("utm", async ({ eq }) => {
   console.time("utm");
   const url =
-    "https://s3-us-west-2.amazonaws.com/planet-disaster-data/hurricane-harvey/SkySat_Freeport_s03_20170831T162740Z3.tif";
+    "https://storage.googleapis.com/pdd-stac/disasters/hurricane-harvey/0831/20170831_172754_101c_3b_Visual.tif";
   const geotiff = await GeoTIFF.fromUrl(url);
   const bbox = [-95.33935546875, 28.92163128242129, -95.3173828125, 28.940861769405547];
   const result = await readBoundingBox({
@@ -36,13 +36,12 @@ test("utm", async ({ eq }) => {
     target_height: 256,
     target_width: 256
   });
-  // eq(result.read_bbox, [271940.8, 3201512.8000000003, 274126.4, 3203687.2]);
-  eq(result.read_bbox, [271938.50786516856, 3201512.8761133603, 274126.9033707865, 3203688.325506073]);
-  eq(result.read_window, [1938, 2517, 2280, 2177]);
-  eq(result.selected_image_index, 4);
+  eq(result.selected_image_index, 2);
+  eq(result.read_bbox, [271941,3201507.449748141,274131,3203691.187814824]);
+  eq(result.read_window, [11084,13132,11449,12768]);
   eq(result.srs_of_geotiff, 32615);
   eq(result.data.length, 3);
-  eq(result.width, 342);
-  eq(result.height, 340);
+  eq(result.width, 365);
+  eq(result.height, 364);
   console.timeEnd("utm");
 });
